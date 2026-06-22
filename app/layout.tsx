e6b0d1441a3a9e2dfe33570ba58f2dbe499145d0
berry010626-ctrl <berry010626@gmail.com>
2026-06-22 13:19:02 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "장마 무드 테스트 | MAYBLUE",
-  description: "이 장마, 당신은 어떤 무드로 버티는 사람일까? MAYBLUE 장마 무드 테스트",
+  description: "이번 장마, 나는 어떻게 보내게 될까? MAYBLUE 장마 무드 테스트",
 };
 
 export default function RootLayout({
@@ -17,7 +17,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Pretendard:wght@400;500;600;700;800&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Jua&family=Pretendard:wght@400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
       </head>
